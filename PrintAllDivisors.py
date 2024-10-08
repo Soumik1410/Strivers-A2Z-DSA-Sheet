91efca9722cc558743a8f129d99ea5fdcfa22806
Soumik1410 <soumik1410code@gmail.com>
2024-10-08 15:40:16 +0530

    Update PrintAllDivisors.py

--- PrintAllDivisors.py
+++ PrintAllDivisors.py
@@ -1,17 +1,25 @@
-bool isPalindrome(int x) {
-    if(x<0)
-        return false;
-    int rev = 0, dig, num = x;
-    while(x > 0)
-    {
-        dig = x % 10;
-        if(214748364.7 < rev)
-            return false;
-        rev = rev * 10 + dig;
-        x = x / 10;
-    }
-    if(rev == num)
-        return true;
-    else
-        return false;
-}
+#User function Template for python3
+
+
+class Solution:
+    def sumOfDivisors(self, N):
+    	#code here 
+    	sum = 0
+    	for i in range(N+1):
+    	    if i == 0:
+    	        continue
+            sum = sum + (N // i) * i
+        return sum
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.sumOfDivisors(N)
+        print(ans)
+# } Driver Code Ends
