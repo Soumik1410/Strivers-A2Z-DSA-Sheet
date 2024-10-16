2227e86b0db81e49e6c2006fec25adc16a5577dc
Soumik1410 <soumik1410code@gmail.com>
2024-10-16 23:19:19 +0530

    Create Spiral Matrix.py

---
+++ Spiral Matrix.py
@@ -0,0 +1,40 @@
+class Solution:
+    def spiralOrder(self, mat: List[List[int]]) -> List[int]:
+        ans = []
+    
+        n = len(mat) # no. of rows
+        m = len(mat[0]) # no. of columns
+    
+        # Initialize the pointers reqd for traversal.
+        top = 0
+        left = 0
+        bottom = n - 1
+        right = m - 1
+        while (top <= bottom and left <= right):
+            # For moving left to right
+            for i in range(left, right + 1):
+                ans.append(mat[top][i])
+
+            top += 1
+
+            # For moving top to bottom.
+            for i in range(top, bottom + 1):
+                ans.append(mat[i][right])
+
+            right -= 1
+
+            # For moving right to left.
+            if (top <= bottom):
+                for i in range(right, left - 1, -1):
+                    ans.append(mat[bottom][i])
+
+                bottom -= 1
+
+            # For moving bottom to top.
+            if (left <= right):
+                for i in range(bottom, top - 1, -1):
+                    ans.append(mat[i][left])
+
+                left += 1
+
+        return ans
