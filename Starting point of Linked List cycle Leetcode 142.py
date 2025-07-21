9d59a8e1749575aecefeb59c20c7efd2491261e5
Soumik1410 <soumik1410code@gmail.com>
2025-07-21 11:27:16 +0530

    Create Starting point of Linked List cycle Leetcode 142.py

---
+++ Starting point of Linked List cycle Leetcode 142.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        flag = 0
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                flag = 1
+                break
+        if flag == 0:
+            return None
+        slow = head
+        count = 0
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+        
