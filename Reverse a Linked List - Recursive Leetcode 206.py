fabd7467194785137ec5ad3cbecb3882eb442cd9
Soumik1410 <soumik1410code@gmail.com>
2025-07-21 11:16:30 +0530

    Create Reverse a Linked List - Recursive Leetcode 206.py

---
+++ Reverse a Linked List - Recursive Leetcode 206.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        second = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return second
