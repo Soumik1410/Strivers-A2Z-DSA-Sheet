82ad325798e8f0f9cc59301e2bea358fbbf7ff63
Soumik1410 <soumik1410code@gmail.com>
2025-08-11 13:33:07 +0530

    Create Asteroid Collision Leetcode 735.py

---
+++ Asteroid Collision Leetcode 735.py
@@ -0,0 +1,15 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids:
+            while stack and a < 0 < stack[-1]:
+                if stack[-1] < -a:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -a:
+                    stack.pop()
+                    break
+                break
+            else:
+                stack.append(a)
+        return stack
